42b57d435d71c25a16ab30e2ecf81c8df1aa77c7
Oleg <https://github.com/OlegVladimirovichOleynik>
2016-09-09 13:09:20 +0500

    add validates, simple form, all CRUD

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,49 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def show
+    @cards = Card.find(params[:id])
+  end
+
+  def new
+    @cards = Card.new
+  end
+
+  def edit
+    @cards = Card.find(params[:id])
+  end
+
+  def create
+    @cards = Card.new(cards_params)
+
+    if @cards.save
+      redirect_to @cards
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @cards = Card.find(params[:id])
+
+    if @cards.update(cards_params)
+      redirect_to @cards
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @cards = Card.find(params[:id])
+    @cards.destroy
+
+    redirect_to cards_path
+  end
+
+  private
+  def cards_params
+    params.require(:card).permit(:original_text, :translated_text)
+  end
+
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,6 @@
 class Card < ApplicationRecord
+	validates :original_text, :translated_text, 	presence: true, 
+																							uniqueness: true,
+						 																confirmation: { case_sensitive: false }
+						 																
 end
