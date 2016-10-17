051f7dafe3592f583cf1959093a9d92649c58fdd
OlegVladimirovichOleynik <xakep0k@mail.ru>
2016-10-18 02:57:03 +0300

    simple test

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,20 +1,24 @@
 class Card < ApplicationRecord
-  before_validation :normalize_name, on: :create
-  before_validation do
+  before_validation :normalize_name, on: [:create, :edit, :update] do
     self.created_at = Date.today.next_day(3)
   end
+  scope :rand_cards, -> { where('created_at <= ?', Date.today).order('RANDOM()').first }
 
   validates :original_text, exclusion: { in: :translated_text,
-     message: "is reserved." }
+                                    message: "is reserved."}
 
-  validates :original_text, :translated_text,	presence: true,
+  validates :original_text, :translated_text, presence: true,
                                               uniqueness: true,
                                               confirmation: { case_sensitive: false }
+
+  def update_date
+    update(created_at: 3.days.from_now)
+  end
 
   protected
 
   def normalize_name
-    self.original_text = original_text.mb_chars.downcase.titleize
-    self.translated_text = translated_text.mb_chars.downcase.titleize
+    self.original_text = original_text.mb_chars.strip.downcase.titleize
+    self.translated_text = translated_text.mb_chars.strip.downcase.titleize
   end
 end
