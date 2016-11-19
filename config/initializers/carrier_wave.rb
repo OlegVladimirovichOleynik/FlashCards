e315ef143fb1aabcae883ae689d6fc512d390d66
OlegVladimirovichOleynik <xakep0k@mail.ru>
2016-11-19 13:11:17 +0300

    add servise heroku

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,9 +2,9 @@ unless Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_KEY_ID'],
+      aws_access_key_id:     ENV['AWS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET'],
-      region:                'us-west-2',
+      region:                ENV['AWS_REGION'],
     }
     config.fog_directory = ENV['AWS_BUCKET']
   end
--- db/migrate/20161108193843_create_decks.rb
+++ db/migrate/20161108193843_create_decks.rb
@@ -5,19 +5,9 @@ class CreateDecks < ActiveRecord::Migration[5.0]
       t.integer :user_id
       t.integer :card_id
     end
-  end
-
-  def change
     add_column :cards, :deck_id, :integer
-  end
-
-  def change
     add_column :users, :current_deck_id, :integer
-  end
-
-  def change
     add_index :cards, :user_id
     add_index :cards, :deck_id
   end
-
 end
